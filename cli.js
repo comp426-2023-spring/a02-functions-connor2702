4115215975ddcf28770e2f5949daf867e5517acc
root <root@UNC-PF2A3Z3S>
2023-04-27 11:20:11 -0400

    Added to cli

--- cli.js
+++ cli.js
@@ -1,2 +1,30 @@
 #!/usr/bin/env node
 
+const minimist = require('minimist');
+const moment = require('moment');
+const argument = minimist(process.argv.slice(2));
+
+const timezone = moment.tz.guess();
+
+
+
+
+
+const response = await fetch('URL_GOES_HERE');
+
+const data = await response.json();
+
+
+
+
+
+
+const days = args.d 
+
+if (days == 0) {
+  console.log("today.")
+} else if (days > 1) {
+  console.log("in " + days + " days.")
+} else {
+  console.log("tomorrow.")
+}
