b928d58e7a180261045cc77c72df75b506c610f3
root <root@UNC-PF2A3Z3S>
2023-04-27 12:12:12 -0400

    Changed argument

--- cli.js
+++ cli.js
@@ -2,7 +2,21 @@
 
 const minimist = require('minimist');
 const moment = require('moment');
+
 const argument = minimist(process.argv.slice(2));
+
+
+if (argument.h) {
+    console.log(`Usage: galosh.js [options] -[n|s] LATITUDE -[e|w] LONGITUDE -z TIME_ZONE
+        -h            Show this help message and exit.
+        -n, -s        Latitude: N positive; S negative.
+        -e, -w        Longitude: E positive; W negative.
+        -z            Time zone: uses tz.guess() from moment-timezone by default.
+        -d 0-6        Day to retrieve weather: 0 is today; defaults to 1.
+        -j            Echo pretty JSON from open-meteo API and exit.`);
+    process.exit(0);
+}
+
 
 const timezone = moment.tz.guess();
 
@@ -10,8 +24,7 @@ const timezone = moment.tz.guess();
 
 
 
-const response = await fetch('URL_GOES_HERE');
-
+const response = await fetch('https://api.open-meteo.com/v1/forecast?latitude=35.875&longitude=-79.0&daily=precipitation_hours&temperature_unit=fahrenheit&timezone=America%2FNew_York');
 const data = await response.json();
 
 
@@ -19,7 +32,7 @@ const data = await response.json();
 
 
 
-const days = args.d 
+const days = argument.d 
 
 if (days == 0) {
   console.log("today.")
